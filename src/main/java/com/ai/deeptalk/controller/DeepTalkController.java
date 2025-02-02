495816c84c464f2ca9f14bafd35dd098f7a40304
nikhil <kumarnikil220@gmail.com>
2025-02-02 23:29:55 +0530

    Calling the api by using WebClient to get the response from the server

--- src/main/java/com/ai/deeptalk/controller/DeepTalkController.java
+++ src/main/java/com/ai/deeptalk/controller/DeepTalkController.java
@@ -4,6 +4,7 @@ import com.ai.deeptalk.services.QnaService;
 import lombok.AllArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +16,9 @@ import java.util.Map;
 @RequestMapping("/api/query")
 public class DeepTalkController {
 
-
     private final QnaService qnAService;
 
+    @PostMapping("/ask")
     public ResponseEntity<String> askQuestion(@RequestBody Map<String,String> payload){
         String question=payload.get("question");
         String answer=qnAService.getAnswer(question);
--- src/main/java/com/ai/deeptalk/services/QnaService.java
+++ src/main/java/com/ai/deeptalk/services/QnaService.java
@@ -2,6 +2,9 @@ package com.ai.deeptalk.services;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @Service
 public class QnaService {
@@ -9,12 +12,39 @@ public class QnaService {
     //Access the APIKEY and API URL
     @Value("${deeptalk.api.url}")
     private String ApiUrl;
-    @Value("{$deeptalk.api.key}")
+    @Value("${deeptalk.api.key}")
     private String ApiKey;
+
+    private final WebClient webClient;
+
+    public QnaService(WebClient.Builder webClient) {
+        this.webClient = webClient.build();
+    }
 
 
     public String getAnswer(String question) {
-        return "";
+        //construct the request payload
+        Map<String,Object> requestBody=Map.of(
+                "contents",new Object[]{
+                        Map.of(
+                                "parts",new Object[]{
+                                        Map.of("text",question)
+                                }
+                        )
+                }
+        );
+
+        //make API Calls
+        String response=webClient.post()
+                .uri(ApiUrl+ApiKey)
+                .header("Content-Type","application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        //return response
+        return response;
     }
 
 
